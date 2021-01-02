f103780487ac1967700e14699197bf6ad7c6cb3e
Pat Hacker <pat.hacker@hey.com>
2021-01-02 10:41:05 -0800

    refector show_error_messages into app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def show_error_messages(model, model_name)
+    return '' unless model.errors.any?
+
+    str = '<div id="error_explanation">'
+    str += "<h2>#{pluralize(model.errors.count, 'error')} prohibited this #{model_name} from being saved:</h2>"
+    str += '<ul>'
+    model.errors.each { |err| str += "<li>#{err.full_message}</li>" }
+    str += '</ul>'
+    str += '</div>'
+    str
+  end
 end
